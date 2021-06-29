b8c8193ba8f644713575acabbf4c704e314f05ef
EmissaryofEmpowerment <41085617+EmissaryofEmpowerment@users.noreply.github.com>
2021-06-29 17:39:53 -0600

    Add files via upload
    probably an IDE or IOS problem... I'll see what my professor says

--- main.cpp
+++ main.cpp
@@ -1,10 +1,60 @@
 #include "payroll_record.h"
 #include <iostream>
+#include <array>
 
 
 using namespace std;
 
-int main() {
+int main()
+{
+    string userInput = "";
+    //Perhaps lines 12-32 would be better if contained in a method in payroll_record.cpp
+    cout << "Please enter the name of the file where the data is stored\r" << endl;
+    cin >> userInput;
+    if (userInput.find(".") < userInput.size() -4)
+    {
+        userInput = userInput.substr(0, userInput.find("."));
+    }
+    if (userInput[(userInput.size()-4)] != '.' )
+    {
+        userInput += ".txt";
+    }
+    else if (userInput.substr((userInput.size() -4), userInput.size()) != ".txt"
+    && userInput.substr((userInput.size() -4), userInput.size()) != ".doc")
+    {
+        userInput.replace(userInput.find("."), 4, ".txt");
+    }
+   userInput.insert(0, "../../");
+    ifstream  in(userInput);
+    if (in.fail())
+    {
+        cout << userInput << " is not a valid file." << endl;
+    }
 
+    //Perhaps lines 35-52 would be better contained in payroll_record.cpp
+    array<PayrollRecord, 32> payrolls;
+    int counter = 0;
+    ofstream out("../../payroll_data.txt");
+    ofstream outReport("../../payroll_report.txt");
+    while (in.good() == true)
+    {
+        double input = 0.00;
+        string strInput = "";
+        in >> input;
+        payrolls[counter].set_hours(input);
+        in >> input;
+        payrolls[counter].set_pay_rate(input);
+        getline(in, strInput);
+        getline(in, strInput); //get past the \r hurdle
+        payrolls[counter].set_name(strInput);
+        payrolls[counter].WriteData(out);
+        payrolls[counter].WriteReport(outReport);
+        counter++;
+        if (counter > payrolls.size())
+        {
+            std::cout << "Number of entries: " << counter << std::endl;
+            break;
+        }
+    }
   return 0;
 }
--- payroll_record.h
+++ payroll_record.h
@@ -1,18 +1,31 @@
-#ifndef PayRoll_H
-#define PayRoll_H
+#ifndef payroll_record_H
+#define payroll_record_H
 #include <string>
-class PayRoll
+#include <fstream>
+
+class PayrollRecord
 {
-private:
-    double hours_;
-    double pay_rate_;
-    std::string first_name;
-    std::string last_name;
 public:
+    PayrollRecord();
+    PayrollRecord(double hours, double pay_rate,std::string name);
+    ~PayrollRecord();
     static const double kMinimumWage;
     static const double kRegularHours;
     static const double kOvertimeRate;
-
-
+    double get_hours() const {return hours_;}
+    double get_pay_rate() const {return pay_rate_;}
+    std::string get_first_name() const {return first_name_;};
+    std::string get_last_name() const {return last_name_;};
+    void set_hours(double hours);
+    void set_pay_rate(double pay_rate);
+    void set_name(std::string name);
+    double ComputeGross() const;
+    void WriteData(std::ostream& out) const;
+    void WriteReport(std::ostream& outReport) const;
+private:
+    double hours_;
+    double pay_rate_;
+    std::string first_name_;
+    std::string last_name_;
 };
 #endif
